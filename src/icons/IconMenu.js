5c5ce0c538fad5f299b15d9999b2ffbd3b0c0c1d
Gorka <gorka@aquigorka.com>
2018-12-12 10:27:52 +0100

    Icons: add menu icon

---
+++ src/icons/IconMenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = props => (
+  <svg width="18px" height="14px" viewBox="0 0 18 14" {...props}>
+    <g transform="translate(-19.000000, -25.000000)" stroke="currentColor">
+      <g transform="translate(20.000000, 25.000000)">
+        <path d="M0,1 L16,1" />
+        <path d="M0,7 L16,7" />
+        <path d="M0,13 L16,13" />
+      </g>
+    </g>
+  </svg>
+)
+
+export default Menu
